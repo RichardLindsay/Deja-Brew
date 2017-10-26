8b17fa3276114837a2b1aa4ea4f69f9c2f96f290
Richard Lindsay <richard@tictocfamily.com>
2017-10-26 15:19:55 +0100

    Tidy up the shuffle method
    Add hop function
    
    Add yeast function

--- index.js
+++ index.js
@@ -3,10 +3,22 @@
 const axios = require('axios');
 const brauhaus = require('brauhaus');
 
+const shuffleArray = function(array) {
+  for (let x = array.length - 1; x >= 0; x--) {
+    let randomIndex = Math.floor(Math.random() * (x + 1));
+    let itemAtIndex = array[randomIndex];
+    array[randomIndex] = array[x];
+    array[x] = itemAtIndex;
+  }
+
+  return array;
+};
+
 const getBeers = function() {
   const beerIds = Array.from({length: 5}, () => Math.floor(Math.random() * 234) + 1).join('|');
   console.log(beerIds);
   return axios.get(`https://api.punkapi.com/v2/beers?ids=${beerIds}`)
+  // return axios.get(`https://api.punkapi.com/v2/beers?ids=109`)
   .then(response => {
     return response;
   })
@@ -26,14 +38,7 @@ const createBeerName = function(data) {
 
   let beerName = '';
 
-
-  // Shuffle the words array
-  for (let w = words.length - 1; w >= 0; w--) {
-    let randomIndex = Math.floor(Math.random() * (w + 1));
-    let itemAtIndex = words[randomIndex];
-    words[randomIndex] = words[w];
-    words[w] = itemAtIndex;
-  }
+  words = shuffleArray(words);
 
   // Use the first 3 words in the shuffled array for the name
   for(let i = 0; i < 3; i++) {
@@ -44,35 +49,88 @@ const createBeerName = function(data) {
 
 };
 
-const getFermentables = function(data) {
-  // console.log(data[0].ingredients.malt);
-  const baseMalt = Array.from(data, v => v.ingredients.malt[0]);
-  const secondaryMalt = Array.from(data, v => v.ingredients.malt[1]);
-  const tertiaryMalt = Array.from(data, v => v.ingredients.malt[2]);
+const getMalt = function(data, idx) {
+  let malts = Array.from(data, v => v.ingredients.malt[idx]);
+  malts = shuffleArray(malts);
+  let malt = malts[0];
+  return malt;
+};
 
+const getHop = function(data, idx) {
+  let hops = Array.from(data, v => v.ingredients.hops[idx]);
+  hops = shuffleArray(hops);
+  let hop = hops[0];
+  return hop;
+};
 
-  console.log(baseMalt[0]);
-  console.log(secondaryMalt[0]);
-  console.log(tertiaryMalt[0]);
-
-
+const getYeast = function(data) {
+  let yeasts = Array.from(data, v => v.ingredients.yeast);
+  yeasts = shuffleArray(yeasts);
+  let yeast = yeasts[0];
+  return yeast;
 };
 
 getBeers().then((response) => {
   console.log(`Requests left: ${getRateLimit(response.headers)}`);
-  console.log(`Name: ${createBeerName(response.data)}`);
-  console.log(`Fermentables: ${getFermentables(response.data)}`);
 
+  console.log(getMalt(response.data, 2));
 
   // Create recipe
-  // let r = new brauhaus.Recipe({
-  //   name: createBeerName(response.data),
-  //   description: '🤷‍',
-  //   batchSize: 20.0,
-  //   boilSize: 10.0
-  // });
-  //
-  // console.log(r);
+  let r = new brauhaus.Recipe({
+    name: createBeerName(response.data),
+    description: '🤷‍',
+    batchSize: 20.0,
+    boilSize: 10.0
+  });
 
+  // Add malts
+  r.add('fermentable', {
+    name: getMalt(response.data, 0).name,
+    weight: getMalt(response.data, 0).amount.value
+  });
+
+  r.add('fermentable', {
+    name: getMalt(response.data, 1).name,
+    weight: getMalt(response.data, 1).amount.value
+  });
+
+  let tertiaryMalt = getMalt(response.data, 2);
+
+  if (typeof tertiaryMalt !== 'undefined') {
+    r.add('fermentable', {
+      name: tertiaryMalt.name,
+      weight: tertiaryMalt.amount.value
+    });
+  }
+
+  // Add hops
+  r.add('hop', {
+    name: getHop(response.data, 0).name,
+    weight: getHop(response.data, 0).amount.value,
+    time: 60
+  });
+
+  r.add('hop', {
+    name: getHop(response.data, 1).name,
+    weight: getHop(response.data, 1).amount.value,
+    time: 30
+  });
+
+  let tertiaryHop = getHop(response.data, 2);
+
+  if (typeof tertiaryHop !== 'undefined') {
+    r.add('hop', {
+      name: tertiaryHop.name,
+      weight: tertiaryHop.amount.value,
+      time: 0
+    });
+  }
+
+  // Add yeast
+  r.add('yeast', {
+    name: getYeast(response.data)
+  });
+
+  console.log(r);
 
 });
