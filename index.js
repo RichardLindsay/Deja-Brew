c809645ca95560ad839859abb5f7be3e6bc71846
Richard Lindsay <richard@tictocfamily.com>
2017-10-26 12:24:35 +0100

    Start to build up recipe

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const axios = require('axios');
+const brauhaus = require('brauhaus');
 
 const getBeers = function() {
   const beerIds = Array.from({length: 5}, () => Math.floor(Math.random() * 234) + 1).join('|');
+  console.log(beerIds);
   return axios.get(`https://api.punkapi.com/v2/beers?ids=${beerIds}`)
   .then(response => {
     return response;
@@ -19,7 +21,6 @@ const getRateLimit = function(data) {
 
 const createBeerName = function(data) {
   const originalNames = Array.from(data, v => v.name).toString();
-  // console.log(originalNames);
   let words = originalNames.replace(/,/g, ' ').replace(/-/g, '').replace(/\((.*?)\)/g, '');
   words = words.split(' ').filter(v => v !== '');
 
@@ -28,13 +29,11 @@ const createBeerName = function(data) {
 
   // Shuffle the words array
   for (let w = words.length - 1; w >= 0; w--) {
-    var randomIndex = Math.floor(Math.random() * (w + 1));
-    var itemAtIndex = words[randomIndex];
+    let randomIndex = Math.floor(Math.random() * (w + 1));
+    let itemAtIndex = words[randomIndex];
     words[randomIndex] = words[w];
     words[w] = itemAtIndex;
   }
-
-  // console.log(words);
 
   // Use the first 3 words in the shuffled array for the name
   for(let i = 0; i < 3; i++) {
@@ -45,12 +44,35 @@ const createBeerName = function(data) {
 
 };
 
-const createBeerDescription = function() {
-  return '🤷';
+const getFermentables = function(data) {
+  // console.log(data[0].ingredients.malt);
+  const baseMalt = Array.from(data, v => v.ingredients.malt[0]);
+  const secondaryMalt = Array.from(data, v => v.ingredients.malt[1]);
+  const tertiaryMalt = Array.from(data, v => v.ingredients.malt[2]);
+
+
+  console.log(baseMalt[0]);
+  console.log(secondaryMalt[0]);
+  console.log(tertiaryMalt[0]);
+
+
 };
 
 getBeers().then((response) => {
   console.log(`Requests left: ${getRateLimit(response.headers)}`);
   console.log(`Name: ${createBeerName(response.data)}`);
-  console.log(`Description: ${createBeerDescription()}`);
+  console.log(`Fermentables: ${getFermentables(response.data)}`);
+
+
+  // Create recipe
+  // let r = new brauhaus.Recipe({
+  //   name: createBeerName(response.data),
+  //   description: '🤷‍',
+  //   batchSize: 20.0,
+  //   boilSize: 10.0
+  // });
+  //
+  // console.log(r);
+
+
 });
